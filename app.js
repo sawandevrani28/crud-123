82ea206abed930940e91acac3a0b24f46b9eeb94
sawandevrani28 <sawan.devrani28@gmail.com>
2025-05-25 18:30:25 -0400

    test

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const port = 3000;
-a
+
 // Create connection to MySQL
 const db = mysql.createConnection({
     host: 'mysql',
